67d8eb5cdc742fecda75240d47ca4e5e01b245b2
teabunniecodes <97705887+teabunniecodes@users.noreply.github.com>
2025-03-29 22:15:27 -0700

    Time: 3 ms (79.92%), Space: 18 MB (21.07%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_int_dict = {"I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000}
+        prev = None
+        ans = 0
+
+        for x in s:
+            curr = roman_int_dict[x]
+            if prev is None:
+                prev = roman_int_dict[x]
+                ans = prev
+            else:
+                if prev >= curr:
+                    prev = curr
+                    ans += prev
+                    print(ans)
+                elif prev < curr:
+                    ans = ans + curr - 2*prev
+                    prev = curr - prev
+                    
+        return ans
