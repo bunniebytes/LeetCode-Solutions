0cb6c679dcc746305b09e35d4c2fb1cef7c65a5b
teabunniecodes <97705887+teabunniecodes@users.noreply.github.com>
2025-03-31 23:19:03 -0700

    Time: 28 ms (66.83%), Space: 21.2 MB (33.62%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        _len = len(nums)
+        remainder_dict = {0:1}
+        prefix_sum = 0
+        ans = 0
+        if _len > 0:
+            for num in nums:
+                prefix_sum += num
+                mod = prefix_sum % k
+                # Python mod will always be positive, but other languages need following if statment if mod is negative.
+                # if mod < 0:
+                #     mod += k
+                if remainder_dict.get(mod):
+                    ans += remainder_dict[mod]
+                    remainder_dict[mod] += 1
+                else:
+                    remainder_dict[mod] = 1
+
+                
+
+        return ans
