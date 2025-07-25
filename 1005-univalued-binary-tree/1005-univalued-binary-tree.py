43145e2e8e6667c72f4c7bdbcd6bf4479b574725
teabunniecodes <97705887+teabunniecodes@users.noreply.github.com>
2025-07-24 23:05:43 -0800

    Time: 3 ms (4.27%), Space: 17.9 MB (15.77%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.py
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -7,15 +7,15 @@
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
         self.root_val = root.val
-        self.return_val = True
-        return self.nodeValuesEqual(root) and self.return_val
+        return self.nodeValuesEqual(root)
         
     def nodeValuesEqual(self, node):
         if node is None:
             return True
-        if node.val == self.root_val:
-            self.nodeValuesEqual(node.left)
-            self.nodeValuesEqual(node.right)
-        else:
-            self.return_val = False
-        return True
+        # print(node.val == self.root_val)
+        # if node.val == self.root_val:
+        #     self.nodeValuesEqual(node.left)
+        #     self.nodeValuesEqual(node.right)
+        # else:
+        #     return False
+        return node.val == self.root_val and self.nodeValuesEqual(node.left) and self.nodeValuesEqual(node.right)
