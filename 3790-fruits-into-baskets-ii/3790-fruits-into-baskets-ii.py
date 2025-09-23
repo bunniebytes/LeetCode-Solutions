6961045547229be582b0c3d6b81190885fd58781
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2025-09-22 22:17:31 -0700

    Time: 24 ms (84.4%), Space: 12.4 MB (87.86%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
+        num_baskets = len(baskets)
         for fruit in fruits:
             pointer = 0
-            while pointer < len(baskets):
+            while pointer < num_baskets:
                 if fruit > baskets[pointer]:
                     pointer += 1
                 elif fruit <= baskets[pointer]:
                     del baskets[pointer]
+                    num_baskets -= 1
                     break
-        return len(baskets)
+        return num_baskets
