6754ac585f51e580b13914a82d7e940b20e87192
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2026-05-01 22:59:34 -0700

    Time: 10 ms (25.89%), Space: 25.1 MB (6.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = defaultdict(int)
+        counts_array = [[] for _ in range(len(nums) + 1)]
         result = []
         # loop through and add count each time I see a number
         for num in nums:
             counts[num] += 1
+        # loop through key and input and append to list at index
+        for num, count in counts.items():
+            counts_array[count].append(num)
 
-        # sort values and slice to k
-        values_sorted = set(sorted(counts.values(), reverse = True)[:k])
-
-        # loop through k, v in dictionary and if in list add to result
-        for key, val in counts.items():
-            if val in values_sorted:
-                result.append(key)
-
-        return result
-
-        
+        # iterate over the list of lists(called item) in reverse
+        for item in counts_array[::-1]:
+            result += item
+            if len(result) == k:
+                return result
