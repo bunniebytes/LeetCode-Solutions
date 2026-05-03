399ca28c5987138c506c9e499d2a714908c5bb44
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2026-05-02 22:30:21 -0700

    Time: 0 ms (100%), Space: 19.4 MB (15.24%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,4 +1,21 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        double_s = s + s
-        return goal in double_s and len(goal) == len(s)
+        goal_dict = defaultdict(list)
+        first_letter = s[0]
+        # check len(s) and len(g) are same
+        if len(s) != len(goal):
+            return False
+        if len(s) == 1:
+            return s == goal
+        # loop through and get dictionary of goals letters and indexes
+        for x in range(len(s)):
+            goal_dict[goal[x]].append(x)
+        # get first letter of s and look up in dictionary and get list of indexes
+        first_letter_indices = goal_dict.get(first_letter)
+        # loop through and slice/Frankenstein goal and check if they equal
+        if first_letter_indices:
+            for idx in first_letter_indices:
+                frankenstein = goal[idx:] + goal[:idx]
+                if s == frankenstein:
+                    return True
+        return False
