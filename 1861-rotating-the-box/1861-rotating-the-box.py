d143844207ed08e7516987e389314ca73abc2e31
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2026-05-05 22:38:18 -0700

    Time: 185 ms (21.5%), Space: 57.4 MB (5.88%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,21 +1,21 @@
 class Solution:
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         # stones = 0
-        rotate_dict = defaultdict(list)
+        num_col = len(boxGrid)
+        num_row = len(boxGrid[0])
+        rotated_list = [[] for x in range(num_row)]
 
         # loop through row list
         for col_idx, row in enumerate(boxGrid):
             stones = 0
             for row_idx, cell in enumerate(row):
-                rotate_dict[row_idx].insert(0, cell)
+                rotated_list[row_idx].insert(0, cell)
                 if cell == "#":
                     stones += 1
                 elif cell == "." and stones > 0:
-                    rotate_dict[row_idx][0] = "#"
-                    print(stones)
-                    print(row_idx - stones)
-                    rotate_dict[row_idx - stones][0] = "."
+                    rotated_list[row_idx][0] = "#"
+                    rotated_list[row_idx - stones][0] = "."
                 else:
                     stones = 0
 
-        return list(rotate_dict.values())
+        return rotated_list
