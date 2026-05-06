7d6d2f0884005f9ef363dd7679345f3843dbf9dd
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2026-05-06 14:36:36 -0700

    Time: 3 ms (82.71%), Space: 20.4 MB (89.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums_len = len(nums)
-        for x in range(nums_len):
-            y = x + 1
-            if nums[x] == 0 and x != nums_len - 1:
-                # check if next num is not 0
-                while y < nums_len - 1:
-                    if nums[y] == 0:
-                        y += 1
-                    else:
-                        break
-                nums[x] = nums[y]
-                nums[y] = 0
-                        
+        zeroes = 0
+        for idx, x in enumerate(nums):
+            if x == 0:
+                zeroes += 1
+            else:
+                nums[idx] = 0
+                nums[idx - zeroes] = x
