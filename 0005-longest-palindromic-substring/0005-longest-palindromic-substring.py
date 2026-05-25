2045684a38f617878ff4a020af285880f81b3434
bunniebytes <97705887+bunniebytes@users.noreply.github.com>
2026-05-24 22:29:12 -0700

    Time: 908 ms (29.19%), Space: 19.4 MB (20.64%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        len_str = len(s)
+        longest_pal = ""
+        reversed_str = s[::-1]
+
+        # loop through first in range(len)
+        for first in range(len_str):
+            for last in reversed(range(len_str)):
+                if s[first] == s[last]:
+                    curr_str = s[first:last + 1]
+                    curr_reversed = reversed_str[len_str - (last + 1):(len_str - first)]
+                    if curr_str == curr_reversed:
+                        if len(curr_str) > len(longest_pal):
+                            longest_pal = curr_str
+                        if len(longest_pal) > (len_str - first):
+                            return longest_pal
+
+        return longest_pal
